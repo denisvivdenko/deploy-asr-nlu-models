55a98cd124d6b7616a799984e57fa9531520b3ac
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2023-09-05 21:52:08 +0200

    feat: add two utils to convert asr model to onnx and inference it using onnxruntime

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(filename: str) -> list[str]:
 
 
 setup(
-    name="sales",
+    name="asr-nlu",
     version="0.0.1",
     packages=find_packages(),
     install_requires=get_requirements("requirements.txt")
