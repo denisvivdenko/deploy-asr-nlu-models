b115633fa83e1089672d361b1daca0f7371c3a1f
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2023-09-06 08:12:51 +0200

    feat: add two stages to dvc

---
+++ src/stages/convert_asr_model_to_onnx.py
@@ -0,0 +1,28 @@
+import argparse
+import logging
+
+from src.utils.configuration import load_params
+from src.utils.convert_to_onnx import convert_wav2vec2_model2onnx
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        "--config",
+        dest="config",
+        type=str,
+    )
+    args = argparser.parse_args()
+    config_fpath = args.config
+
+    params = load_params(config_fpath)
+
+    logging.info(f"""Start converting wav2vec2 model to onnx.\
+    Model {params['convert_asr_model']['input_model_path_or_id']}
+    will be saved to {params['convert_asr_model']['output_model_path']}
+    """)
+    convert_wav2vec2_model2onnx(
+        params["convert_asr_model"]["input_model_path_or_id"],
+        params["convert_asr_model"]["output_model_path"]
+    )
+    logging.info("Finished converting wav2vec2 model to onnx")
---
+++ src/stages/convert_nlu_model_to_onnx.py
@@ -0,0 +1,28 @@
+import argparse
+import logging
+
+from src.utils.configuration import load_params
+from src.utils.convert_to_onnx import convert_nlu_model2onnx
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        "--config",
+        dest="config",
+        type=str,
+    )
+    args = argparser.parse_args()
+    config_fpath = args.config
+
+    params = load_params(config_fpath)
+
+    logging.info(f"""Start converting nlu model to onnx.\
+    Model {params['convert_asr_model']['input_model_path_or_id']}
+    will be saved to {params['convert_asr_model']['output_model_path']}
+    """)
+    convert_nlu_model2onnx(
+        params["convert_asr_model"]["input_model_path_or_id"],
+        params["convert_asr_model"]["output_model_path"]
+    )
+    logging.info("Finished converting nlu model to onnx")
